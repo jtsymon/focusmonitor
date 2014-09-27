36e2d855099c471145a2c6e897019f3b56b992d5
Jeremy Symon <jtsymon@gmail.com>
2014-09-27 22:07:58 +1200

    Now also updates when the active window's title changes

--- focusmonitor.c
+++ focusmonitor.c
@@ -12,17 +12,29 @@ int main() {
     XSelectInput(d, DefaultRootWindow(d), PropertyChangeMask);
 
     XEvent e;
-    Window w;
+    Window w = 0;
     int revert_to;
+    char *atom;
 
     for (;;) {
         // print current focused window
         XGetInputFocus(d, &w, &revert_to);
+        // also listen for events on the active window
+        XSelectInput(d, w, PropertyChangeMask);
         printf("%llu\n", (int)w);
         // wait for next event
         do {
             XNextEvent(d, &e);
-        } while (e.type != PropertyNotify || strcmp(XGetAtomName(d, e.xproperty.atom), "_NET_ACTIVE_WINDOW"));
+        } while (
+                e.type != PropertyNotify ||
+                !(atom = XGetAtomName(d, e.xproperty.atom)) ||
+                (
+                 strcmp(atom, "_NET_ACTIVE_WINDOW") &&
+                 strcmp(atom, "WM_ICON_NAME")
+                )
+        );
+        // stop listening for events on the active window
+        XSelectInput(d, w, NoEventMask);
     }
     return 0;
 }
